13ac3f68bd3bec05c702bdad9191e45dd7d9aea9
伟丰陈 <836594824@qq.com>
2016-10-11 11:28:27 +0800

    lala

---
+++ lala/lala/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lala
+//
+//  Created by 小陈 on 16/10/11.
+//  Copyright © 2016年 小陈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lala/lala/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lala
+//
+//  Created by 小陈 on 16/10/11.
+//  Copyright © 2016年 小陈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
